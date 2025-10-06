37e5323b1225b8d6f725b7dd810d87545cfd2d22
Zachary Gazaille <zacharygazaille@gmail.com>
2025-10-06 15:05:51 -0400

    New attempt to fix CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,9 @@ import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from database import clear_database
+from database import init_database, clear_database
 
 @pytest.fixture(autouse=True)
 def run_before_each_test():
+    init_database()
     clear_database() # Clears DB before running tests so that tests work without having to switch all the book IDs every time
